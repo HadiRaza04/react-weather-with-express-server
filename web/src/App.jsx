7ed294697f86789cb42cbe6d9db80f03cd49a113
Hadi Raza <hadiraza.edu@gmail.com>
2022-12-31 17:01:11 +0500

    data from server

---
+++ web/src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useState } from "react"
+import axios from "axios";
+
+let baseUrl = ``;
+if (window.location.href.split(":")[0] === "http"){
+    baseUrl = "http://locallhost:5001";
+}
+
+
+function App() {
+
+    const [weatherData, setWeatherData] = useState(null)
+    const [cityName, setCityName] = useState("")
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log("I am click handler")
+        axios.get(`${baseUrl}/weather`)
+            .then(response => {
+                console.log("response: ", response.data);
+
+                setWeatherData(response.data);
+            })
+            .catch(err => {
+                console.log("error: ", err);
+            })
+    }
+
+    return (
+        <div>
+
+            <form onSubmit={submitHandler}>
+                City Name:
+                <input type="text" placeholder="enter your city name" required
+                    onChange={(e) => { setCityName(e.target.value) }} />
+
+                <button type="submit">get weather</button>
+            </form>
+            <br />
+            <br />
+
+            {(weatherData === null) ? null :
+                <div>
+                    temperature: {Math.round(weatherData?.temp)}°C
+                    <br />
+                    min: {Math.round(weatherData?.min)}°C
+                    <br />
+                    max: {Math.round(weatherData?.max)}°C
+                    <br />
+                    humadity: {Math.round(weatherData?.humadity)}°C
+                </div>
+            }
+        </div>
+    );
+}
+
+export default App;
+
+
+
